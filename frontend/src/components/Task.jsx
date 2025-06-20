20b4d598b5fe4e03e8bfd77b434c90d29e4dd01e
Alex Pumari <alexpumari0@gmail.com>
2025-06-20 19:26:57 -0300

    feat(ListOfTasks): add list of tasks component

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Checkbox from '../components/CheckBox'
+import Checkbox from './CheckBox'
 
 export default function Task ({ checked, title, className = '' }) {
   const [st, setSt] = useState(checked)
@@ -7,7 +7,7 @@ export default function Task ({ checked, title, className = '' }) {
      setSt(e.target.checked)
   }
   return (
-    <li className={`flex items-center gap-x-4 ${className}`}>
+    <li className={`flex items-center gap-x-4 py-3 ${className}`}>
       <Checkbox checked={st} onChange={handleChange} />
       <span>{title}</span>
     </li>
