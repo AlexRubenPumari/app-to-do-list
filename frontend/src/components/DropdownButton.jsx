71d290df0a53121a34affcc246223c2df2c629cf
AlexRubenPumari <alexpumari0@gmail.com>
2025-06-21 19:57:16 -0300

    feat(components): add dropdown button component

---
+++ frontend/src/components/DropdownButton.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import { IconArrow } from './Icons'
+import PrimaryButton from './PrimaryButton'
+
+export default function DropdownButton ({
+  currentValue, items, callbacks, className = ''
+}) {
+  const [toggleMenu, setToggleMenu] = useState(false)
+  useEffect(() => {
+    const closeMenu = () => setToggleMenu(false)
+    window.addEventListener('click', closeMenu)
+
+    return () => window.removeEventListener('click', closeMenu) 
+  }, [])
+  return (
+    <PrimaryButton
+      className={`inline-flex gap-2 relative ${className}`}
+      onClick={e => (e.stopPropagation() ,setToggleMenu(!toggleMenu))}
+    >
+      <span className='w-20 text-left'>{currentValue}</span>
+      <span>|</span>
+      <IconArrow className='w-4' />
+      {toggleMenu && (
+        <Menu
+          className='absolute inset-x-0 top-[calc(100%_+_3px)] z-50'
+          items={items}
+          callbacks={callbacks}
+        />
+      )}
+    </PrimaryButton>
+  )
+}
+
+export function Menu ({ items, callbacks, className = '' }) {
+  return (
+    <ul
+      className={`bg-white text-black rounded-md text-left overflow-hidden ${className}`}
+    >
+      {items.map(item => (
+        <li
+          key={item}
+          className='px-4 py-2 hover:bg-violet-400 transition-colors'
+          onClick={callbacks[item]}>{item}
+        </li>
+      ))}
+    </ul>
+  )
+}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import Searchbar from '../components/Searchbar'
 import ListOfTasks from '../components/ListOfTasks'
-import PrimaryButton from '../components/PrimaryButton'
+import DropdownButton from '../components/DropdownButton'
 import CircleButton from '../components/CircleButton'
 
 export default function HomePage() {
@@ -8,7 +8,7 @@ export default function HomePage() {
     <div className='flex flex-col gap-4'>
       <h2 className='text-2xl font-bold text-center'>TODO LIST</h2>
       <Searchbar placeholder='Search task...' />
-      <PrimaryButton>Filtro</PrimaryButton>
+      <Filter />
       <ListOfTasks />
       <CircleButton
         className='fixed right-4 bottom-4 text-2xl font-bold w-12 h-12'
@@ -16,5 +16,20 @@ export default function HomePage() {
           +
       </CircleButton>
     </div>
+  )
+}
+// import DropdownButton from '../components/DropdownButton'
+
+export function Filter () {
+  const filterItems = ['All', 'Pendientes', 'Terminadas']
+  const filterCallbacks = {All: () => console.log(1), Pendientes: () => console.log(2), Terminadas: () => console.log(3)}
+  
+  return (
+    <DropdownButton
+      currentValue={'All'}
+      className='self-start'
+      items={filterItems}
+      callbacks={filterCallbacks}
+    />
   )
 }
