704ce345d2649507103bdae336bacf7f590f25a7
AlexRubenPumari <alexpumari0@gmail.com>
2025-06-21 19:45:42 -0300

    feat(components): add meatballs and kebab icon to icons components

--- frontend/src/components/Icons.jsx
+++ frontend/src/components/Icons.jsx
@@ -24,3 +24,19 @@ export function IconArrow({ className = '' }) {
     </svg>
   )
 }
+
+export function IconKebab ({ className = '' }) {
+  return (
+    <svg viewBox='10 3 4 18' className={className}>
+      <path d='M12,6 C12.5522847,6 13,5.55228475 13,5 C13,4.44771525 12.5522847,4 12,4 C11.4477153,4 11,4.44771525 11,5 C11,5.55228475 11.4477153,6 12,6 Z' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeDasharray='0,0' />
+      <path d='M12,13 C12.5522847,13 13,12.5522847 13,12 C13,11.4477153 12.5522847,11 12,11 C11.4477153,11 11,11.4477153 11,12 C11,12.5522847 11.4477153,13 12,13 Z' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeDasharray='0,0' />
+      <path d='M12,20 C12.5522847,20 13,19.5522847 13,19 C13,18.4477153 12.5522847,18 12,18 C11.4477153,18 11,18.4477153 11,19 C11,19.5522847 11.4477153,20 12,20 Z' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeDasharray='0,0' />
+    </svg>
+  )
+}
+
+export function IconMeatballs ({ className = '' }) {
+  return (
+    <IconKebab className={`rotate-90 ${className}`} />
+  )
+}
