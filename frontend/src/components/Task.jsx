e58618eda173ee2afe31eed2eccefb84159f10a0
Alex Pumari <alexpumari0@gmail.com>
2025-06-21 02:28:12 -0300

    fix(components): fix conditional styles in task component

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -3,13 +3,16 @@ import Checkbox from './CheckBox'
 
 export default function Task ({ checked, title, className = '' }) {
   const [st, setSt] = useState(checked)
+  const titleClasses = st
+    ? 'text-gray-500 line-through'
+    : ''
   const handleChange = e => {
      setSt(e.target.checked)
   }
   return (
     <li className={`flex items-center gap-x-4 py-3 ${className}`}>
       <Checkbox checked={st} onChange={handleChange} />
-      <span>{title}</span>
+      <span className={titleClasses}>{title}</span>
     </li>
   )
 }
