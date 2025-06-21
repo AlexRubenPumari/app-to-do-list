ee17501e6fefd3dbba6b9f3690083467e381a37a
AlexRubenPumari <alexpumari0@gmail.com>
2025-06-21 20:29:48 -0300

    feat(components): add close button component

---
+++ frontend/src/components/CloseButton.jsx
@@ -0,0 +1,9 @@
+export default function ButtonClose({ className = '', onClick }) {
+  const btnClassName =
+    `leading-[1rem] p-5 font-bold text-3xl hover:text-gray-500 ${className}`
+  return (
+    <button className={btnClassName} onClick={onClick}>
+      ×
+    </button>
+  )
+}
