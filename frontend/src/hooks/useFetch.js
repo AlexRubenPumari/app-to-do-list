c653065bfc2e46e0c6ca62ef2d58fc57becb359e
AlexRubenPumari <alexpumari0@gmail.com>
2025-06-22 12:58:20 -0300

    feat(hooks): add useFetch custom hook

---
+++ frontend/src/hooks/useFetch.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+
+export function useFetch ({ url, asyncFunction }) {
+  const [data, setData] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    setIsLoading(true)
+
+    asyncFunction(url)
+      .then(data => setData(data))
+      .catch(errorCode => setError(errorCode))
+      .finally(() => setIsLoading(false))
+  }, [url])
+
+  return { data, isLoading, error }
+}
