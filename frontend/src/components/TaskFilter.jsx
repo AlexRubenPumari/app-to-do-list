ed1f61c68de524d22bd33ff10344aa0de3975b0c
AlexRubenPumari <alexpumari0@gmail.com>
2025-06-21 23:54:30 -0300

    feat(components): add task modal and task filter components

---
+++ frontend/src/components/TaskFilter.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { FILTER_ITEMS } from '../config/constants'
+import DropdownButton from '../components/DropdownButton'
+
+export default function TaskFilter () {
+  const [value, setValue] = useState(FILTER_ITEMS[0])
+  const filterCallbacks = {
+    [FILTER_ITEMS[0]]: () => setValue(FILTER_ITEMS[0]),
+    [FILTER_ITEMS[1]]: () => setValue(FILTER_ITEMS[1]),
+    [FILTER_ITEMS[2]]: () => setValue(FILTER_ITEMS[2])
+  }
+  
+  return (
+    <DropdownButton
+      currentValue={value}
+      className='self-start'
+      items={FILTER_ITEMS}
+      callbacks={filterCallbacks}
+    />
+  )
+}
---
+++ frontend/src/components/TaskModal.jsx
@@ -0,0 +1,37 @@
+import Modal from './Modal'
+
+export default function TaskModal ({ Task, onClose }) {
+  const firstColClasses = 'text-gray-700'
+  const { title, completed, description, createdAt } = Task
+  return (
+    <Modal
+      size='md'
+      className='flex-grow flex-center-all flex-col gap-4'
+      onClose={onClose}
+    >
+      <h2 className='w-full text-center text-xl font-bold uppercase'>{title}</h2>
+      <table>
+        <tbody>
+          <tr>
+            <td className={firstColClasses}>Status</td>
+            <td className='font-bold'>
+              {
+              !completed
+                ? <span className='text-red-600'>Pending</span>
+                : <span className='text-green-700'>Finished</span>
+              }
+              </td>
+          </tr>
+          <tr>
+            <td className={firstColClasses}>Date</td>
+            <td>{createdAt}</td>
+          </tr>
+          <tr>
+            <td className={firstColClasses}>Description</td>
+            <td>{description}</td>
+          </tr>
+        </tbody>
+      </table>
+    </Modal>
+  )
+}
