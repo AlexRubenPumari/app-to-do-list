d78bf6c423a3982684f8819db5553392c5cf4331
AlexRubenPumari <alexpumari0@gmail.com>
2025-06-21 23:30:48 -0300

    feat(components): add size options to modal component

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,13 +1,14 @@
 import CloseButton from './CloseButton'
 
-export default function Modal ({ children, className, onClose }) {
+export default function Modal ({ children, size = 'lg', className, onClose }) {
+  const SIZES = { sm: 'h-[28vh]', md: 'h-[55vh]', lg: 'h-[90vh]' }
   return (
     <div
       className='fixed inset-0 z-10 bg-black/60 flex-center-all'
       onClick={onClose}
     >
       <div
-        className='w-[40rem] max-w-[85vw] h-[90vh] px-5 py-7 flex flex-col bg-slate-200 rounded-lg'
+        className={`w-[40rem] max-w-[85vw] px-5 py-7 flex flex-col bg-slate-200 rounded-lg ${SIZES[size]}`}
         onClick={e => e.stopPropagation()}
       >
         <CloseButton className='self-end' onClick={onClose} />
