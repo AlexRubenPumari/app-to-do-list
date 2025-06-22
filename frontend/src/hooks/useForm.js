68beee064802eef0ce23439e6a07c87e7dabc1a6
AlexRubenPumari <alexpumari0@gmail.com>
2025-06-22 18:47:06 -0300

    feat(hooks): add useForm custom hook

---
+++ frontend/src/hooks/useForm.js
@@ -0,0 +1,44 @@
+import { useState, useCallback } from 'react'
+
+export function useForm (initialValues, keyloggers, validations) {
+  const [values, setValues] = useState(initialValues)
+  const [errors, setErrors] = useState({})
+
+  const handleChange = useCallback((e) => {
+    const { name, value } = e.target
+    const lastChar = value.slice(-1)
+    if (keyloggers && !keyloggers[name]?.(lastChar)) return
+
+    setValues(prevValues => ({ ...prevValues, [name]: value }))
+    setErrors(prevErrors => ({ ...prevErrors, [name]: null }))
+  }, [])
+
+  const validateForm = useCallback(() => {
+    let newErrors = {}
+    let formIsValid = true
+
+    for (const fieldName in validations) {
+      const validate = validations[fieldName]
+      const value = values[fieldName]
+      const error = validate(value)
+
+      if (error) {
+        newErrors[fieldName] = error
+        formIsValid = false
+      }
+    }
+
+    setErrors(newErrors)
+    return formIsValid
+  }, [values, validations])
+
+
+  return {
+    values,
+    errors,
+    handleChange,
+    validateForm,
+    setValues,
+    setErrors,
+  }
+}
